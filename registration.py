f8aad138a4b31280bd547877cf20323d551aff7a
Estefania Mesquita <estefanialunardi@gmail.com>
2022-05-30 12:02:24 -0300

    bug

--- registration.py
+++ registration.py
@@ -282,98 +282,91 @@ try:
 except:
     st.error("Quelque chose s'est mal passé. Réessayez plus tard!")
     
+#connect_to_tunnel_and_mysqlserver  
+load_dotenv()
+db_server = st.secrets["db_server"]
+user = st.secrets["user"]
+db_port = st.secrets["db_port"]
+password = st.secrets["password"]
+ip = st.secrets["ip"]
+db_name = st.secrets["db_name"]
+ip_ssh = st.secrets["ip_ssh"]
+ssh_username = st.secrets["ssh_username"]
+ssh_password = st.secrets["ssh_password"]
+remote_bind_address = st.secrets["remote_bind_address"]
+#db_server= os.getenv('db_server')
+#user=os.getenv("user")
+#db_port=os.getenv("db_port")
+#password=os.getenv("password")
+#ip=os.getenv("ip")
+#db_name=os.getenv("db_name")
+#ip_ssh=os.getenv("ip_ssh")
+#ssh_username=os.getenv("ssh_username")
+#ssh_password=os.getenv("ssh_password")
+#remote_bind_address=os.getenv("remote_bind_address")
 try:
-    
-    #connect_to_tunnel_and_mysqlserver  
-    load_dotenv()
-
-    db_server = st.secrets["db_server"]
-    user = st.secrets["user"]
-    db_port = st.secrets["db_port"]
-    password = st.secrets["password"]
-    ip = st.secrets["ip"]
-    db_name = st.secrets["db_name"]
-    ip_ssh = st.secrets["ip_ssh"]
-    ssh_username = st.secrets["ssh_username"]
-    ssh_password = st.secrets["ssh_password"]
-    remote_bind_address = st.secrets["remote_bind_address"]
-
-
-    #db_server= os.getenv('db_server')
-    #user=os.getenv("user")
-    #db_port=os.getenv("db_port")
-    #password=os.getenv("password")
-    #ip=os.getenv("ip")
-    #db_name=os.getenv("db_name")
-    #ip_ssh=os.getenv("ip_ssh")
-    #ssh_username=os.getenv("ssh_username")
-    #ssh_password=os.getenv("ssh_password")
-    #remote_bind_address=os.getenv("remote_bind_address")
-    try:
-        server = SSHTunnelForwarder((ip_ssh, 4242), ssh_username=ssh_username, ssh_password=ssh_password, remote_bind_address=(db_server, 3306))
-        server.start()
-        port = str(server.local_bind_port)
-        conn_addr = 'mysql://' + user + ':' + password + '@' + db_server + ':' + port + '/' + db_name
-        engine = create_engine(conn_addr)
-        connection = engine.connect()
-    except:
-        st.error("Quelque chose s'est mal passé. Réessayez plus tard! 1")
-    try:
-        mySql_insert_query0 = f"""UPDATE elevesdf set name = '{name}', birthday='{birthday}', age='{age}', address='{address}', city='{city}', toulouse = '{toulouse}', cpode='{pcode}',lat='{lat}',long='{lon}', mail='{mail}', telephone = '{telephone}', legal_representative= '{legal_representative}' where `name` = '{name}'"""
-        engine.execute(mySql_insert_query0)
-        st.spinner(text="S'il vous plaît, attendez !")
-    except: 
-        mySql_insert_query1 = f"""INSERT INTO elevesdf (name, birthday, age, address, city, toulouse, pcode, lat, `long`, mail, telephone, legal_representative) VALUES ('{name}', '{birthday}', {age}, '{address}', '{city}', '{toulouse}', {pcode},{lat}, {lon}, '{mail}', '{telephone}', '{legal_representative}')"""
-        engine.execute(mySql_insert_query1)
-        st.spinner(text="Veuillez patienter pendant que nous enregistrons vos informations !")
-    try: 
-        mySql_insert_query2 = f"""INSERT INTO coursdf22 (name, course, schedule, course2, schedule2, course3, schedule3) VALUES ('{name}', '{course}', '{schedule}','{course2}', '{schedule2}','{course3}', '{schedule3}'); """
-        engine.execute(mySql_insert_query2)
-    except:
-        st.error("Quelque chose s'est mal passé. Réessayez plus tard!2 ")
-    try: 
-        mySql_insert_query3 = f"""INSERT INTO paimentsdf22 (name, registration, installments, total) VALUES  ('{name}', '{registration}', '{installments}', '{total}');"""
-        engine.execute(mySql_insert_query3)
-    except:
-        st.error("Quelque chose s'est mal passé. Réessayez plus tard!3 ")
-    try:
-        courses = pd.read_sql_query("""SELECT name, course FROM coursdf22 UNION ALL SELECT name, course2 FROM coursdf22 UNION ALL SELECT name, course3 FROM coursdf22""",conn_addr)
-        courses_filled=[]
-        name_filled=[]
-        for row in range(len(courses['course'])):
-            if courses['course'].iloc[row] !='0' and courses['course'].iloc[row]  != "":
-                courses_filled.append(courses['course'].iloc[row])
-                name_filled.append(courses['name'].iloc[row])
-        course_filled = pd.DataFrame(zip(courses_filled, name_filled))
-        course_filled.columns = ['course', 'name']
-        course_filled.to_sql('course_filled', conn_addr, if_exists='replace', index=False)
-    except:
-        st.error("Quelque chose s'est mal passé. Réessayez plus tard!")
-    #bytes_medical = certificat_medical.getvalue()
-    #sql_blob_query = f""" INSERT INTO medical23 VALUES (%s,%s)""" 
-    #engine.execute(sql_blob_query, ['2',memoryview(bytes_medical)])
-    #bytes_dassurance = certificat_dassurance.getvalue()
-    #sql_blob_query = f""" INSERT INTO assurance23 VALUES (%s,%s)""" 
-    #engine.execute(sql_blob_query, ['2',memoryview(bytes_dassurance)])    
-    connection.close()  
-    server.stop()
-    #my_email= os.getenv('my_email')
-    #mail_password= os.getenv('mail_password')
-    #msg=MIMEText(f"""{name} , 
-    #votre inscription à Attitude Corps et Danses a été reçue! En cas de problème concernant les informations ou les fichiers fournis, nous vous contacterons !
-    #Rendez-vous en classe !""")
-    #msg['Subject']= f" {name}, votre inscription à Attitude Corps et Danses !"
-    #msg['From']= my_email
-    #msg["To"]= f'{mail}, {my_email}'
-    #mail_server = smtplib.SMTP_SSL('smtp.gmail.com' ,465)
-    #mail_server.ehlo()
-    #mail_server.login(my_email, mail_password)
-    #mail_server.sendmail(msg["From"], msg["To"], msg.as_string())
-    #mail_server.close()
-    st.success("Merci! Rendez-vous en classe !")
-    st.balloons()
+    server = SSHTunnelForwarder((ip_ssh, 4242), ssh_username=ssh_username, ssh_password=ssh_password, remote_bind_address=(db_server, 3306))
+    server.start()
+    port = str(server.local_bind_port)
+    conn_addr = 'mysql://' + user + ':' + password + '@' + db_server + ':' + port + '/' + db_name
+    engine = create_engine(conn_addr)
+    connection = engine.connect()
 except:
-    st.error("Quelque chose s'est mal passé. Réessayez plus tard!")    
+    st.error("Quelque chose s'est mal passé. Réessayez plus tard! 1")
+try:
+    mySql_insert_query0 = f"""UPDATE elevesdf set name = '{name}', birthday='{birthday}', age='{age}', address='{address}', city='{city}', toulouse = '{toulouse}', cpode='{pcode}',lat='{lat}',long='{lon}', mail='{mail}', telephone = '{telephone}', legal_representative= '{legal_representative}' where `name` = '{name}'"""
+    engine.execute(mySql_insert_query0)
+    st.spinner(text="S'il vous plaît, attendez !")
+except: 
+    mySql_insert_query1 = f"""INSERT INTO elevesdf (name, birthday, age, address, city, toulouse, pcode, lat, `long`, mail, telephone, legal_representative) VALUES ('{name}', '{birthday}', {age}, '{address}', '{city}', '{toulouse}', {pcode},{lat}, {lon}, '{mail}', '{telephone}', '{legal_representative}')"""
+    engine.execute(mySql_insert_query1)
+    st.spinner(text="Veuillez patienter pendant que nous enregistrons vos informations !")
+try: 
+    mySql_insert_query2 = f"""INSERT INTO coursdf22 (name, course, schedule, course2, schedule2, course3, schedule3) VALUES ('{name}', '{course}', '{schedule}','{course2}', '{schedule2}','{course3}', '{schedule3}'); """
+    engine.execute(mySql_insert_query2)
+except:
+    st.error("Quelque chose s'est mal passé. Réessayez plus tard!2 ")
+try: 
+    mySql_insert_query3 = f"""INSERT INTO paimentsdf22 (name, registration, installments, total) VALUES  ('{name}', '{registration}', '{installments}', '{total}');"""
+    engine.execute(mySql_insert_query3)
+except:
+    st.error("Quelque chose s'est mal passé. Réessayez plus tard!3 ")
+try:
+    courses = pd.read_sql_query("""SELECT name, course FROM coursdf22 UNION ALL SELECT name, course2 FROM coursdf22 UNION ALL SELECT name, course3 FROM coursdf22""",conn_addr)
+    courses_filled=[]
+    name_filled=[]
+    for row in range(len(courses['course'])):
+        if courses['course'].iloc[row] !='0' and courses['course'].iloc[row]  != "":
+            courses_filled.append(courses['course'].iloc[row])
+            name_filled.append(courses['name'].iloc[row])
+    course_filled = pd.DataFrame(zip(courses_filled, name_filled))
+    course_filled.columns = ['course', 'name']
+    course_filled.to_sql('course_filled', conn_addr, if_exists='replace', index=False)
+except:
+    st.error("Quelque chose s'est mal passé. Réessayez plus tard!")
+#bytes_medical = certificat_medical.getvalue()
+#sql_blob_query = f""" INSERT INTO medical23 VALUES (%s,%s)""" 
+#engine.execute(sql_blob_query, ['2',memoryview(bytes_medical)])
+#bytes_dassurance = certificat_dassurance.getvalue()
+#sql_blob_query = f""" INSERT INTO assurance23 VALUES (%s,%s)""" 
+#engine.execute(sql_blob_query, ['2',memoryview(bytes_dassurance)])    
+connection.close()  
+server.stop()
+#my_email= os.getenv('my_email')
+#mail_password= os.getenv('mail_password')
+#msg=MIMEText(f"""{name} , 
+#votre inscription à Attitude Corps et Danses a été reçue! En cas de problème concernant les informations ou les fichiers fournis, nous vous contacterons !
+#Rendez-vous en classe !""")
+#msg['Subject']= f" {name}, votre inscription à Attitude Corps et Danses !"
+#msg['From']= my_email
+#msg["To"]= f'{mail}, {my_email}'
+#mail_server = smtplib.SMTP_SSL('smtp.gmail.com' ,465)
+#mail_server.ehlo()
+#mail_server.login(my_email, mail_password)
+#mail_server.sendmail(msg["From"], msg["To"], msg.as_string())
+#mail_server.close()
+st.success("Merci! Rendez-vous en classe !")
+st.balloons()
 
 
 
