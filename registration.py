63ab05ee392841bcbcc1718921fb428dab4b1eba
Estefania Mesquita <estefanialunardi@gmail.com>
2023-08-04 09:46:03 -0300

    message

--- registration.py
+++ registration.py
@@ -351,15 +351,15 @@ try:
                         classes_student.append(0)
                 #connect_to_tunnel_and_mysqlserver  
                 #load_dotenv()
-                dbserver = st.secrets["db_server"]
+                db_server = st.secrets["db_server"]
                 user = st.secrets["user"]
                 db_port = st.secrets["db_port"]
                 password = st.secrets["password"]
                 ip = st.secrets["ip"]
                 db_name = st.secrets["db_name"]
-                ipssh = st.secrets["ip_ssh"]
-                sshusername = st.secrets["ssh_username"]
-                sshpassword = st.secrets["ssh_password"]
+                ip_ssh = st.secrets["ip_ssh"]
+                ssh_username = st.secrets["ssh_username"]
+                ssh_password = st.secrets["ssh_password"]
                 #db_server= os.getenv('db_server')
                 #user=os.getenv("user")
                 #db_port=os.getenv("db_port")
@@ -371,12 +371,13 @@ try:
                 #ssh_password=os.getenv("ssh_password")
                 #remote_bind_address=os.getenv("remote_bind_address")
                 try:
-                    server = SSHTunnelForwarder((ipssh, 4242), ssh_username=sshusername, ssh_password=sshpassword, remote_bind_address=(dbserver, 3306))
+                    server = SSHTunnelForwarder((ip_ssh, 4242), ssh_username=ssh_username, ssh_password=ssh_password, remote_bind_address=(db_server, 3306))
                     server.start()
                     port = str(server.local_bind_port)
-                    conn_addr = 'mysql://' + user + ':' + password + '@' + dbserver + ':' + port + '/' + db_name
+                    conn_addr = 'mysql://' + user + ':' + password + '@' + db_server + ':' + port + '/' + db_name
                     engine = create_engine(conn_addr)
                     connection = engine.connect()
+                    st.success("Connected!")
                 except:
                     st.error("Quelque chose s'est mal passé. Réessayez plus tard! 1")
                 try:
